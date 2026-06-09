f3e80ce4e5b9968f8ee536e33a00dfa42460805a
Kate Williamscd <katiew23@gmail.com>
2026-06-09 20:20:17 +0100

    add to favourites logic completed: dummy favourites page

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -11,10 +11,10 @@ import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
-import img from '../../images/film-poster-placeholder.png';
-import { BaseMovieProps } from "../../types/interfaces"; 
+import Avatar from "@mui/material/Avatar";
+import img from "../../images/film-poster-placeholder.png";
+import { BaseMovieProps } from "../../types/interfaces";
 import { Link } from "react-router-dom";
-
 
 const styles = {
   card: { maxWidth: 345 },
@@ -24,49 +24,77 @@ const styles = {
   },
 };
 
-const MovieCard: React.FC<BaseMovieProps> = (movie) => {
-  
-  
+interface MovieCardProps {
+  movie: BaseMovieProps;
+  selectFavourite: (movieId: number) => void;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie, selectFavourite }) => {
+  const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    selectFavourite(movie.id);
+  };
+
   return (
     <Card sx={styles.card}>
-    <CardHeader title={movie.title} />
-    <CardMedia
-    sx={styles.media}
-    image={
-      movie.poster_path
-      ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-      : img
-    }
-    />
-    <CardContent>
-    <Grid container>
-    <Grid item xs={6}>
-    <Typography variant="h6" component="p">
-    <CalendarIcon fontSize="small" />
-    {movie.release_date}
-    </Typography>
-    </Grid>
-    <Grid item xs={6}>
-    <Typography variant="h6" component="p">
-    <StarRateIcon fontSize="small" />
-    {"  "} {movie.vote_average}{" "}
-    </Typography>
-    </Grid>
-    </Grid>
-    </CardContent>
-    <CardActions disableSpacing>
-    <IconButton aria-label="add to favorites" >
-    <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
-    <Link to={`/movies/${movie.id}`}>
-    <Button variant="outlined" size="medium" color="primary">
-    More Info ...
-    </Button>
-    </Link>
-    
-    </CardActions>
+      <CardHeader
+        avatar={
+          movie.favourite ? (
+            <Avatar sx={styles.avatar}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
+        title={
+          <Typography variant="h5" component="p">
+            {movie.title}{" "}
+          </Typography>
+        }
+      />
+
+      <CardMedia
+        sx={styles.media}
+        image={
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+            : img
+        }
+      />
+
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" />
+              {movie.release_date}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" />
+              {"  "} {movie.vote_average}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+
+      <CardActions disableSpacing>
+        <IconButton
+          aria-label="add to favourites"
+          onClick={handleAddToFavourite}
+        >
+          <FavoriteIcon color="primary" fontSize="large" />
+        </IconButton>
+
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+      </CardActions>
     </Card>
   );
-}
+};
 
 export default MovieCard;
