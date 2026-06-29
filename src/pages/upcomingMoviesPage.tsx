7351d181ff348aec7739cee39aed05c1d9d36193
Kate Williamscd <katiew23@gmail.com>
2026-06-29 19:25:51 +0100

    final lecture exercise 1

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import MovieList from "../components/movieList";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import { BaseMovieProps } from "../types/interfaces";
-
+import { BaseMovieProps, DiscoverMovies } from "../types/interfaces";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>(
+    "upcoming",
+    getUpcomingMovies
+  );
 
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-  }, []);
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data ? data.results : [];
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
       action={(movie: BaseMovieProps) => {
-        return <AddToFavoritesIcon {...movie} />;
+        return <AddToFavouritesIcon {...movie} />;
       }}
-    >
-      <MovieList movies={movies} />
-    </PageTemplate>
+    />
   );
 };
 
