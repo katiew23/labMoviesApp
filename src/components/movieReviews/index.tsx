a53ab184f18aeeadc8f90c4a6f95b2e15c09d66a
Kate Williamscd <katiew23@gmail.com>
2026-07-02 19:08:45 +0100

    upgraded to TanStack Query and add paginated API Listing page

--- src/components/movieReviews/index.tsx
+++ src/components/movieReviews/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,61 +7,74 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { getMovieReviews } from "../../api/tmdb-api";
 import { excerpt } from "../../util";
-
-import { MovieDetailsProps, Review } from "../../types/interfaces"; // Import the MovieT type from the appropriate location
+import { MovieDetailsProps, Review } from "../../types/interfaces";
+import Spinner from "../spinner";
 
 const styles = {
-    table: {
-        minWidth: 550,
-    },
+  table: {
+    minWidth: 550,
+  },
 };
 
-const MovieReviews: React.FC<MovieDetailsProps> = (movie) => { 
-    const [reviews, setReviews] = useState([]);
+const MovieReviews: React.FC<MovieDetailsProps> = (movie) => {
+  const {
+    data: reviews,
+    error,
+    isPending,
+    isError,
+  } = useQuery<Review[], Error>({
+    queryKey: ["reviews", movie.id],
+    queryFn: () => getMovieReviews(movie.id),
+  });
 
-    useEffect(() => {
-        getMovieReviews(movie.id).then((reviews) => {
-            setReviews(reviews);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  if (isPending) {
+    return <Spinner />;
+  }
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={styles.table} aria-label="reviews table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell >Author</TableCell>
-                        <TableCell align="center">Excerpt</TableCell>
-                        <TableCell align="right">More</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {reviews.map((r: Review) => (
-                        <TableRow key={r.id}>
-                            <TableCell component="th" scope="row">
-                                {r.author}
-                            </TableCell>
-                            <TableCell >{excerpt(r.content)}</TableCell>
-                            <TableCell >
-                                <Link
-                                    to={`/reviews/${r.id}`}
-                                    state={{
-                                        review: r,
-                                        movie: movie,
-                                    }}
-                                >
-                                    Full Review
-                                </Link>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-}
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={styles.table} aria-label="reviews table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Author</TableCell>
+            <TableCell align="center">Excerpt</TableCell>
+            <TableCell align="right">More</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {reviews.map((r: Review) => (
+            <TableRow key={r.id}>
+              <TableCell component="th" scope="row">
+                {r.author}
+              </TableCell>
+
+              <TableCell>{excerpt(r.content)}</TableCell>
+
+              <TableCell>
+                <Link
+                  to={`/reviews/${r.id}`}
+                  state={{
+                    review: r,
+                    movie: movie,
+                  }}
+                >
+                  Full Review
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default MovieReviews;
